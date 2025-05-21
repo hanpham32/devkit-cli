06b141ca899cc93ea82eedc0ae540d64812c00fa
Grezle <4310551+grezle@users.noreply.github.com>
2025-05-21 17:36:38 +0100

    fix: expose ssh agent to unblock build step (#113)
    <!-- 
        🚨 ATTENTION! 🚨 
        
    This PR template is REQUIRED. PRs not following this format will be
    closed without review.
        
        Requirements:
    - PR title must follow commit conventions:
    https://www.conventionalcommits.org/en/v1.0.0/
    - Label your PR with the correct type (e.g., 🐛 Bug, ✨ Enhancement, 🧪
    Test, etc.)
        - Provide clear and specific details in each section
    -->
    
    **Motivation:**
    <!--
    Explain here the context, and why you're making that change. What is the
    problem you're trying to solve.
    -->
    Since adding `set -e` to `build.sh` our e2e has been failing on the
    build step as we do not have an ssh-agent running.
    
    **Modifications:**
    <!--
    Describe the modifications you've done from a high level. What are the
    key technical decisions and why were they made?
    -->
    - adds a deploy key to our repo
    - starts and authenticates an ssh-agent
    
    **Result:**
    <!--
    *After your change, what will change.
    -->
    - fixes build step in e2e test

--- pkg/template/config_test.go
+++ pkg/template/config_test.go
@@ -17,7 +17,7 @@ func TestLoadConfig(t *testing.T) {
 	}
 
 	expectedBaseURL := "https://github.com/Layr-Labs/hourglass-avs-template"
-	expectedVersion := "v0.0.4"
+	expectedVersion := "v0.0.5"
 
 	if mainBaseURL != expectedBaseURL {
 		t.Errorf("Unexpected main template base URL: got %s, want %s", mainBaseURL, expectedBaseURL)
