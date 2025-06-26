c82801a19bc097846fdc2b391109300b94fc3b10
Grezle <4310551+grezle@users.noreply.github.com>
2025-06-26 14:58:08 +0100

    fix: set base-fee and gas-price to simulate L2 (#208)
    <!-- 
        🚨 ATTENTION! 🚨 
        
    This PR template is REQUIRED. PRs not following this format will be
    closed without review.
        
        Requirements:
    - PR title must follow commit conventions:
    https://www.conventionalcommits.org/en/v1.0.0/
    - Label your PR with the correct type (e.g., 🐛 Bug, ✨ Enhancement, 🧪
    Test, etc.)
        - Provide clear and specific details in each section
    -->
    
    **Motivation:**
    <!--
    Explain here the context, and why you're making that change. What is the
    problem you're trying to solve.
    -->
    As a devkit user, I want my devnet gas fees to simulate a cheap L2.
    
    **Modifications:**
    <!--
    Describe the modifications you've done from a high level. What are the
    key technical decisions and why were they made?
    -->
    - sets `base-fee` = `0`
    - sets `gas-price` = `1000000`
    
    **Result:**
    <!--
    *After your change, what will change.
    -->
    - gas fees reflect a cheap L2
    
    **Testing:**
    <!--
    *List testing procedures taken and useful artifacts.
    -->
    - tested manually
    
    <img width="1211" alt="Screenshot 2025-06-26 at 00 42 39"
    src="https://github.com/user-attachments/assets/d36c3171-c901-412d-8f5e-5b34be0da3ed"
    />

--- pkg/common/devnet/constants.go
+++ pkg/common/devnet/constants.go
@@ -2,7 +2,7 @@ package devnet
 
 // Foundry Image Date : 21 April 2025
 const FOUNDRY_IMAGE = "ghcr.io/foundry-rs/foundry:stable"
-const CHAIN_ARGS = "--gas-limit 140000000 --base-fee 9400000"
+const CHAIN_ARGS = "--gas-limit 140000000 --base-fee 0 --gas-price 1000000"
 const FUND_VALUE = "1000000000000000000"
 const DEVNET_CONTEXT = "devnet"
 const L1 = "l1"
